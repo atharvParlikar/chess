1cba97e40dc9b9468b6869ed1bee1ca7cdd484b3
Atharv Parlikar <atharvparlikar@gmail.com>
2023-05-29 20:54:40 +0530

    fixed a bunch of things and made game more durable

--- backend/main.js
+++ backend/main.js
@@ -14,17 +14,46 @@ const io = new Server(httpServer, {
 app.use(cors());
 
 const connections = {}
+const sides = {}
+let fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+// TODO(for later): validate moves on server too to check if the move by correct side is made
+//                  for example white should not emit black's moves.
+//
+// TODO: write logic for randomly diciding player color/side.
+// TODO: keep the state of bord same even after reload.
 
 io.on('connect', (socket) => {
-  console.log(`Socket connected ${socket.id}`)
+  console.log(`Socket connected ${socket.id}`);
   connections[socket.id] = socket;
+
+  socket.emit('fen', fen);
+
+  if (Object.keys(sides) === 0)
+    sides[socket.id] = Math.random() < 0.5 ? 'white' : 'black';
+  else
+    sides[socket.id] = sides[Object.keys(sides)[0]] === 'white' ? 'black' : 'white';
+
+  for (const socId of Object.keys(sides)) {
+    connections[socId].emit('side', sides[socId]);
+  }
+
   socket.on('s-move', (move) => {
     const conn_keys = Object.keys(connections);
     const socketIndex = conn_keys.indexOf(socket.id);
     const opponentSocketId = conn_keys.slice(socketIndex - 1)[0];
     connections[opponentSocketId].emit('r-move', move);
   });
+  
+  socket.on('fen', (fen_) => fen = fen_);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket disconnected ${socket.id}`);
+    delete connections[socket.id];
+    delete sides[socket.id];
+  });
 });
+
 
 httpServer.listen(3000, () => {
   console.log('Server is live on port: 3000');
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,36 +7,43 @@ import { socket } from './socket';
 const chess = new Chess();
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [isConnected, setIsConnected] = useState(socket.connected); // Dont know why i need this for but i'll keep this here just in case
   const [moves, setMoves] = useState([]);
+  const [side, setSide] = useState('white');
+  const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
   useEffect(() => {
-    const onConnected = () => setIsConnected(true);
-    const onDisconnected = () => setIsConnected(false);
     const onMove = (move) => {
       chess.move(move);
       setFen(chess.fen());
+      socket.emit('fen', chess.fen());
       setMoves([...moves, move]);
     };
 
-    socket.on('connect', onConnected);
-    socket.on('disconnect', onDisconnected);
+    socket.on('connect', () => setIsConnected(true));
+    socket.on('disconnect', () => setIsConnected(false));
     socket.on('r-move', onMove);
+    socket.on('side', (side_) => setSide(side_));
+    socket.on('fen', (fen) => setFen(fen));
 
     return () => {
-      socket.off('connect', onConnected);
-      socket.off('disconnect', onDisconnected);
-      socket.off('r-move', onMove);
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('r-move');
+      socket.off('side')
+      socket.off('fen');
     };
   }, []);
-
-  const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
   const handleDrop = (square) => {
     if (square.sourceSquare === square.targetSquare)
       return;
 
     try {
+      // make sure player is moving their own piece [moved piece is same color as player color]
+      if (chess.get(square.sourceSquare).color !== side[0])
+        return;
+
       chess.move({from: square.sourceSquare, to: square.targetSquare});
       socket.emit('s-move', {from: square.sourceSquare, to: square.targetSquare});
       setFen(chess.fen());
@@ -48,7 +55,7 @@ function App() {
   return (
     <div className="App">
       <div className="board">
-        <Chessboard width={400} position={fen} onDrop={handleDrop} />
+        <Chessboard orientation={side} width={400} position={fen} onDrop={handleDrop} />
       </div>
       <p>Fen: {fen}</p>
     </div>
