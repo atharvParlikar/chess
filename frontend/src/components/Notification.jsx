066db398f0b88d2126fc042b2edc32b89d383f1f
Atharv Parlikar <atharvparlikar@gmail.com>
2023-05-31 03:49:33 +0530

    added framer-motion

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -1,8 +1,15 @@
-const Notification = ({message}) => {
+import { motion } from 'framer-motion';
+
+const Notification = ({ message }) => {
   return (
-    <div className="border-2 border-black p-6 rounded-md bg-gray-300 text-bold font-mono z-10 fixed top-6 right-6 ">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+      className="border-2 border-black p-6 rounded-md bg-gray-300 text-bold font-mono z-10 fixed top-6 right-6"
+    >
       {message}
-    </div>
+    </motion.div>
   );
 }
 
